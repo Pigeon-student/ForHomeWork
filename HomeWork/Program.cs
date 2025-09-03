f1e570ed7965cb930a5bb6cd916c420573da69ee
AndrewPigeon <andrewpigeon904@gmail.com>
2025-09-03 07:50:24 +0300

    Додавання программи "День Тижня" зробленої через switch-case

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,48 @@
-﻿using System;
-Console.WriteLine("Hello, World!");
+﻿// Программа дл отримання назви дня тижня за номером дня тижня. Користувач вводить із клавіатури номер дня тижня (1-7)
+// Необхідно вивести на екран назву дня тижня.
+// Наприклад, якщо "1", на екрані напис "Понеділок", "2" — "Вівторок", тощо.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Введіть номер дня тижня: ");
+        int dayNumber = int.Parse(Console.ReadLine());
+
+        string dayName;
+
+        switch (dayNumber)
+        {
+            case 1:
+                dayName = "Понеділок";
+                break;
+            case 2:
+                dayName = "Вівторок";
+                break;
+            case 3:
+                dayName = "Венсдей Аддамс";
+                break;
+            case 4:
+                dayName = "Кровавий Четвер";
+                break;
+            case 5:
+                dayName = "П'ятниця";
+                break;
+            case 6:
+                dayName = "Субота";
+                break;
+            case 7:
+                dayName = "Неділя";
+                break;
+            default:
+                dayName = "Invalid day number";
+                break;
+
+        }
+        Console.Write($"Ви вибрали - {dayName}");
+    }
+}
